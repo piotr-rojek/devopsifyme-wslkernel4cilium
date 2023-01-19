1a824e37514a19cc03a6e70f9d46732c2ff9ca26
Piotr Rojek <dra9ula@gmail.com>
2023-01-19 11:49:02 +0100

    test build and release

--- Update-Kernel4Cilium.ps1
+++ Update-Kernel4Cilium.ps1
@@ -6,21 +6,21 @@ param(
     $WslKernelPath = "$env:USERPROFILE\.wslkernel",
     $ContainerName = 'cilium',
     $KernelOrigin='https://github.com/microsoft/WSL2-Linux-Kernel.git',
-    $KernelBranch='linux-msft-wsl-5.15.y'
+    $KernelBranch='linux-msft-wsl-5.15.y',
+    $ImageTag = 'ubuntu:latest'
 )
 
 # build the kernel, checkout output folder for logs, kernel and config used
 try
 {
-    New-Item -Type Directory output -Force | Out-Null
-    docker run --name $ContainerName -d -it -e KERNEL_ORIGIN=$KernelOrigin -e KERNEL_BRANCH=$KernelBranch ubuntu:latest bash
+    docker run --name $ContainerName -d -it -e KERNEL_ORIGIN=$KernelOrigin -e KERNEL_BRANCH=$KernelBranch $ImageTag bash
     docker cp build.sh "$($ContainerName):/"
     docker exec $ContainerName sh /build.sh
-    docker cp "$($ContainerName):/output/" output/
+    docker cp "$($ContainerName):/output" .
 }
 finally
 {
-    docker container rm $ContainerName --force
+    #docker rm $ContainerName --force
 }
 
 # copy kernel to user profile
@@ -28,7 +28,7 @@ Copy-Item output/bzImage $WslKernelPath -Force
 
 # update wsl configiration
 Install-Module -Scope CurrentUser PsIni -Force
-New-Item $WslConfigPath -Type File
+New-Item $WslConfigPath -Type File -Force | Out-Null
 $wslConfig = Get-IniContent $WslConfigPath
 $wslConfig.wsl2 ??= @{}
 $wslConfig.wsl2.kernel = $WslKernelPath -replace '\\','\\'
